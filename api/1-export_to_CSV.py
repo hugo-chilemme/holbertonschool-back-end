407f2535bc1bd890cfb86419e8d8901f5b6b5c40
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-07 12:01:34 +0000

    Formatting the script to save tasks in a json

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3
-"""script to get all done task of the user"""
+"""Script to export user tasks to CSV"""
 import requests
 import sys
 
 
+def api_request(event_url):
+    """ API call function """
+    if event_url is not None and type(event_url) is str:
+        url = "https://jsonplaceholder.typicode.com/{}".format(event_url)
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()
+        raise TypeError("The URL returned an error {}".format(r.status_code))
+
+
 if __name__ == '__main__':
-    api_url = "https://jsonplaceholder.typicode.com"
     """script to get all done task of the user"""
     if len(sys.argv) > 1:
         id = sys.argv[1]
-        user = requests.get("{}/users/{}".format(api_url, id))
-        if user.status_code == 200:
-            user = user.json()
-            if len(user) > 0:
-                todos = requests.get("{}/todos?userId={}".format(api_url, id))
-                todos = todos.json()
+
+        # User Recovery
+        user = api_request("users/{}".format(id))
+        if len(user) > 0:
+
+            # User task retrievals
+            todos = api_request("todos?userId={}".format(id))
+            if len(todos) > 0:
+
+                # Inserting data line by line
                 csv = ""
                 for todo in todos:
                     csv += '"{}","{}","{}","{}"\n'.format(
                         todo["userId"],
                         user["username"],
                         todo["completed"],
-                        todo["title"]
-                    )
+                        todo["title"])
+
+                # Remove last newline
                 csv = csv[:-1]
+
+                # Sending data to a .csv file
                 with open("{}.csv".format(id), 'w') as csvfile:
                     csvfile.write(csv)
                     csvfile.close()
