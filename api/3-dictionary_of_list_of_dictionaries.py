3dc068c77f3dbc81769bee17448a129777d33afc
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-07 12:11:56 +0000

    Formatting the script to retrieve all tasks from all users

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,29 +1,54 @@
 #!/usr/bin/python3
-"""script to get all done task of the user"""
+"""Script to export user tasks to JSON file"""
 import json
 import requests
 import sys
 
+
+def api_request(event_url):
+    """ API call function """
+    if event_url is not None and type(event_url) is str:
+        url = "https://jsonplaceholder.typicode.com/{}".format(event_url)
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()
+        raise TypeError("The URL returned an error {}".format(r.status_code))
+
+
 if __name__ == '__main__':
-    api_url = "https://jsonplaceholder.typicode.com"
     """script to get all done task of the user"""
-    user_data = requests.get("{}/users".format(api_url, id))
-    if user_data.status_code == 200:
+
+    # User Recovery
+    users_list = api_request("users")
+    if len(users_list) > 0:
+
+        # Creation of a dictionary to access the user more quickly
         users = {}
-        for user in user_data.json():
+        for user in users_list:
             users[user['id']] = user
-        todos = requests.get("{}/todos".format(api_url))
-        todos = todos.json()
-        callback = {}
-        for todo in todos:
-            userId = todo['userId']
-            if userId not in callback:
-                callback[userId] = []
-            callback[userId].append({
-                "task": todo['title'],
-                "completed": todo['completed'],
-                "username": users[userId]['username']
-            })
-        with open("todo_all_employees.json", 'w') as csvfile:
-            csvfile.write(json.dumps(callback))
-            csvfile.close()
+
+        # Users task retrievals
+        todos = api_request("todos")
+        if len(todos) > 0:
+
+            # Creation of the final table
+            callback = {}
+
+            # We browse data by data to add in the table
+            for todo in todos:
+                userId = todo['userId']
+                # We check that the user table does not exist to create it
+                if userId not in callback:
+                    callback[userId] = []
+
+                # Adding the task in the user table
+                callback[userId].append({
+                    "task": todo['title'],
+                    "completed": todo['completed'],
+                    "username": users[userId]['username']
+                })
+
+            # Sending data to a .json file
+            with open("todo_all_employees.json", 'w') as csvfile:
+                csvfile.write(json.dumps(callback))
+                csvfile.close()
