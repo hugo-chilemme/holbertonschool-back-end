e0fb4751b952642ab7496477f9fc2d2a13fe5dff
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-07 10:11:01 +0000

    Delete space between data in a csv file

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                 todos = todos.json()
                 csv = ""
                 for todo in todos:
-                    csv += '"{}", "{}", "{}", "{}"\n'.format(
+                    csv += '"{}","{}","{}","{}"\n'.format(
                         todo["userId"],
                         user["username"],
                         todo["completed"],
