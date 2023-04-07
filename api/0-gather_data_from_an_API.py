ccf4c41c4549dbfc20450c7af39c772d3a95211e
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-07 11:39:04 +0200

    Fix issue module documented

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,26 +6,23 @@ import sys
 
 if __name__ == '__main__':
     api_url = "https://jsonplaceholder.typicode.com"
-    
     """script to get all done task of the user"""
-    id = sys.argv[1]
-    user = requests.get("{}/users/{}".format(api_url, id))
-    if user.status_code == 200:
-        user = user.json()
-        if len(user) > 0:
-            todos = requests.get("{}/todos?userId={}".format(api_url, id))
-            todos = todos.json()
-            completed = list(filter(lambda todo: todo["completed"], todos))
-            
-            EMPLOYEE_NAME = user['name']
-            NUMBER_OF_DONE_TASKS = len(completed)
-            TOTAL_NUMBER_OF_TASKS = len(todos)
-            
-            print("Employee {} is done with tasks({}/{}):".format(
-                EMPLOYEE_NAME,
-                NUMBER_OF_DONE_TASKS,
-                TOTAL_NUMBER_OF_TASKS
-            ))
-
-            for todo in completed:
-                print("\t {}".format(todo['title']))
+    if len(sys.argv) > 1:
+        id = sys.argv[1]
+        user = requests.get("{}/users/{}".format(api_url, id))
+        if user.status_code == 200:
+            user = user.json()
+            if len(user) > 0:
+                todos = requests.get("{}/todos?userId={}".format(api_url, id))
+                todos = todos.json()
+                completed = list(filter(lambda todo: todo["completed"], todos))
+                EMPLOYEE_NAME = user['name']
+                NUMBER_OF_DONE_TASKS = len(completed)
+                TOTAL_NUMBER_OF_TASKS = len(todos)
+                print("Employee {} is done with tasks({}/{}):".format(
+                    EMPLOYEE_NAME,
+                    NUMBER_OF_DONE_TASKS,
+                    TOTAL_NUMBER_OF_TASKS
+                ))
+                for todo in completed:
+                    print("\t {}".format(todo['title']))
