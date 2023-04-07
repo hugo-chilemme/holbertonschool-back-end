4f0bc55ba28e5e55af23e2c3ca0220847686afa1
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-07 18:14:45 +0000

    PEP8 validation

--- api/api_request.py
+++ api/api_request.py
@@ -3,27 +3,28 @@
 import requests
 from urllib.parse import urlencode
 
-def api_request(event_url, params = {}):
-    """ 
+
+def api_request(event_url, params={}):
+    """
         description:
-            API call function 
+            API call function
         parameters:
             @event_url: contains the target route with possibly parameters
             @params: to pass to the API in object format
     """
-    
+
     # check that the @event_url is correct
     if event_url is None or type(event_url) is not str:
-        
+
         # The request failed, the program is stopped
         raise TypeError(f"The data is missing or incorrect")
-    
+
     # We check if params is valid
     if params is not None and type(params) is dict:
-        
+
         # transformation of the dictionary into a string "?userId=3&..."
         params = "?" + urlencode(params)
-    
+
     # Declaration of the REST API url
     url = f"https://jsonplaceholder.typicode.com/{event_url}{params}"
 
@@ -36,12 +37,11 @@ def api_request(event_url, params = {}):
         try:
             # Returning data in object format
             return r.json()
-        
-        except :
-            
+
+        except TypeError:
+
             # We don't return anything, because it's a rest api, it's json.
             return {}
 
     # The request failed, the program is stopped
     raise TypeError(f"The URL returned an error {r.status_code}")
-    
