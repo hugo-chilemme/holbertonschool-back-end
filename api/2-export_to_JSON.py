32ee02611e63428af646468116dab4f4dfe02562
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-07 12:05:54 +0000

    Formatting the script to save tasks in a json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python3
-"""script to get all done task of the user"""
+"""Script to export user tasks to JSON file"""
 import json
 import requests
 import sys
 
+
+def api_request(event_url):
+    """ API call function """
+    if event_url is not None and type(event_url) is str:
+        url = "https://jsonplaceholder.typicode.com/{}".format(event_url)
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()
+        raise TypeError("The URL returned an error {}".format(r.status_code))
+
+
 if __name__ == '__main__':
-    api_url = "https://jsonplaceholder.typicode.com"
     """script to get all done task of the user"""
     if len(sys.argv) > 1:
         id = sys.argv[1]
-        user = requests.get("{}/users/{}".format(api_url, id))
-        if user.status_code == 200:
-            user = user.json()
-            if len(user) > 0:
-                todos = requests.get("{}/todos?userId={}".format(api_url, id))
-                todos = todos.json()
+
+        # User Recovery
+        user = api_request("users/{}".format(id))
+        if len(user) > 0:
+
+            # User task retrievals
+            todos = api_request("todos?userId={}".format(id))
+            if len(todos) > 0:
+
+                # Create from requested userId: []
                 callback = {}
                 callback[id] = []
+
+                # We browse data by data to add in the table
                 for todo in todos:
                     callback[id].append({
                         "task": todo['title'],
                         "completed": todo['completed'],
                         "username": user['username']
                     })
+
+                # Sending data to a .json file
                 with open("{}.json".format(id), 'w') as csvfile:
                     csvfile.write(json.dumps(callback))
                     csvfile.close()
