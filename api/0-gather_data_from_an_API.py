04bd379fa08ba79367cab1a60fe474e6aade2ee2
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-07 11:32:43 +0200

    Fix issue module documented

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,26 +4,26 @@ import requests
 import sys
 
 
+if __name__ == '__main__':
+    """script to get all done task of the user"""
+    id = sys.argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/'+id)
+    if user.status_code == 200:
+        user = user.json()
+        if len(user) > 0:        
+            todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId='+id)
+            todos = todos.json()
+            completed = list(filter(lambda todo: todo["completed"], todos))
+            
+            EMPLOYEE_NAME = user['name']
+            NUMBER_OF_DONE_TASKS = len(completed)
+            TOTAL_NUMBER_OF_TASKS = len(todos)
+            
+            print("Employee {} is done with tasks({}/{}):".format(
+                EMPLOYEE_NAME,
+                NUMBER_OF_DONE_TASKS,
+                TOTAL_NUMBER_OF_TASKS
+            ))
 
-id = sys.argv[1]
-
-user = requests.get('https://jsonplaceholder.typicode.com/users/'+id)
-if user.status_code == 200:
-    user = user.json()
-    if len(user) > 0:        
-        todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId='+id)
-        todos = todos.json()
-        completed = list(filter(lambda todo: todo["completed"], todos))
-        
-        EMPLOYEE_NAME = user['name']
-        NUMBER_OF_DONE_TASKS = len(completed)
-        TOTAL_NUMBER_OF_TASKS = len(todos)
-        
-        print("Employee {} is done with tasks({}/{}):".format(
-            EMPLOYEE_NAME,
-            NUMBER_OF_DONE_TASKS,
-            TOTAL_NUMBER_OF_TASKS
-        ))
-
-        for todo in completed:
-            print("\t {}".format(todo['title']))
+            for todo in completed:
+                print("\t {}".format(todo['title']))
