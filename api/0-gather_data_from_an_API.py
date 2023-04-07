69f74221a76304f0d208c12a20237099f7ce6088
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-07 11:54:44 +0000

    Refactoring user unmade task recovery

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,25 +4,46 @@ import requests
 import sys
 
 
+def api_request(event_url):
+    """ API call function """
+    if event_url is not None and type(event_url) is str:
+        url = "https://jsonplaceholder.typicode.com/{}".format(event_url)
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()
+        raise TypeError("The URL returned an error {}".format(r.status_code))
+
+
 if __name__ == '__main__':
-    api_url = "https://jsonplaceholder.typicode.com"
-    """script to get all done task of the user"""
+    """ script to get all done task of the user """
     if len(sys.argv) > 1:
         id = sys.argv[1]
-        user = requests.get("{}/users/{}".format(api_url, id))
-        if user.status_code == 200:
-            user = user.json()
-            if len(user) > 0:
-                todos = requests.get("{}/todos?userId={}".format(api_url, id))
-                todos = todos.json()
-                completed = list(filter(lambda todo: todo["completed"], todos))
+
+        # User Recovery
+        user = api_request("users/{}".format(id))
+        if len(user) > 0:
+
+            # User task retrievals
+            todos = api_request("todos?userId={}".format(id))
+            if len(todos) > 0:
+
+                # Recovery of uncompleted tasks
+                tasks_not_done = list(
+                    filter(lambda todo: todo["completed"], todos)
+                )
+
+                # Display Data Initialization
                 EMPLOYEE_NAME = user['name']
-                NUMBER_OF_DONE_TASKS = len(completed)
+                NUMBER_OF_DONE_TASKS = len(tasks_not_done)
                 TOTAL_NUMBER_OF_TASKS = len(todos)
+
+                # Viewing the user's to-do report
                 print("Employee {} is done with tasks({}/{}):".format(
                     EMPLOYEE_NAME,
                     NUMBER_OF_DONE_TASKS,
                     TOTAL_NUMBER_OF_TASKS
                 ))
-                for todo in completed:
+
+                # Displaying the title of each task
+                for todo in tasks_not_done:
                     print("\t {}".format(todo['title']))
